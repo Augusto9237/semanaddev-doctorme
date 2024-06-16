7fd0ddc854471e89f348f11b86dc7660bac7c6ce
Augusto9237 <augusto.souza8330@gmail.com>
2024-06-16 02:33:18 -0300

    feat: add Patient page

--- app/app/patient/page.tsx
+++ app/app/patient/page.tsx
@@ -1,3 +1,21 @@
+import Button from "@/components/button";
+import { Header } from "@/components/header";
+
 export default function Patient() {
-	return <h1>Patient</h1>;
+	return (
+		<>
+			<Header title="Meus Agendamentos" href="/" iconClassName="w-4 h-4" />
+
+			<div className="flex border border-gray-200 p-2 mt-8 mb-9 rounded-xl justify-between items-center">
+				<button className="w-1/2 rounded-xl py-2 px-4 font-semibold bg-green-600 hover:opacity-85">Proximos</button>
+				<button className="w-1/2 rounded-xl py-2 px-4 hover:bg-gray-100">Realizados</button>
+			</div>
+
+			<div>
+				<h2>Hoje</h2>
+				<div>CARD</div>
+				<h2>Amanhã</h2>
+			</div>
+		</>
+	)
 }
