5eafb4222ecada8c972d5135d1d57cad2cb94e2f
Augusto9237 <augusto.souza8330@gmail.com>
2024-06-21 00:23:09 -0300

    feat: add card component

--- app/app/patient/page.tsx
+++ app/app/patient/page.tsx
@@ -4,6 +4,32 @@ import { formartTime } from "@/other/helpers";
 import Image from "next/image";
 
 export default function Patient() {
+	const appointments = [
+		{
+			id: '1',
+			firstName: 'Dr Daniel',
+			photo: 'photo-1.jpg',
+			speciality: 'Clinico Geral',
+			date: '2024-06-15T12:00:00.000Z',
+			made: false
+		},
+		{
+			id: '3',
+			firstName: 'Dr André',
+			photo: 'photo-4.jpg',
+			speciality: 'Cardiologista',
+			date: '2024-06-16T12:00:00.000Z',
+			made: false
+		},
+		{
+			id: '4',
+			firstName: 'Dr Carlos',
+			photo: 'photo-2.jpg',
+			speciality: 'Oftamologista',
+			date: '2024-06-18T12:00:00.000Z',
+			made: false
+		}
+	]
 	return (
 		<>
 			<Header title="Meus Agendamentos" href="/" iconClassName="w-4 h-4" />
@@ -15,8 +41,13 @@ export default function Patient() {
 
 			<div>
 				<h2 className="font-semibold text-lg mb-4">Hoje</h2>
-				<div>{ }</div>
+				<div className="flex flex-col gap-4 mb-5">
+					{appointments.map(item => <Card key={item.id} {...item} />)}
+				</div>
 				<h2 className="font-semibold text-lg mb-4">Amanhã</h2>
+				<div className="flex flex-col gap-4">
+					{appointments.map(item => <Card key={item.id} {...item} />)}
+				</div>
 			</div>
 		</>
 	)
@@ -25,23 +56,22 @@ export default function Patient() {
 type AppointmentProps = {
 	id: string;
 	firstName: string;
-	lastName: string;
 	photo: string;
 	speciality: string;
 	date: string;
-	made: string;
+	made: boolean;
 }
 
 function Card(appointment: AppointmentProps) {
 	return (
-		<div className="flex flex-col">
-			<div>
+		<div className="flex w-full items-center gap-6">
+			<div className="flex flex-col">
 				{formartTime(new Date(appointment.date))}
 				<span className="border-b border-dashed"></span>
 				{formartTime(new Date(appointment.date), true)}
 			</div>
 
-			<div className="flex p-4 rounded-lg bg-green-50 w-full items-center relative">
+			<div className="flex p-4 rounded-lg bg-green-50 w-full items-center relative gap-4">
 				<span className="absolute -m-4 w-[2px] h-[60px] bg-green-500 rounded-r-lg"></span>
 				<div>
 					<Image
